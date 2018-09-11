af3b6d5c73299be37a8209946b78612d7190d226
cyzest <cyzest@nate.com>
2018-09-11 15:48:07 +0900

    Logback 설정 및 Sentry 설정 추가

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM openjdk:8-jdk-alpine
 COPY target/find-books-*.jar app.jar
 
 RUN echo '#!/usr/bin/env sh' >> entrypoint.sh
-RUN echo 'exec java -server -Djava.security.egd=file:/dev/./urandom -DVAULT_TOKEN=$VAULT_TOKEN -jar /app.jar' >> entrypoint.sh
+RUN echo 'exec java -server -Djava.security.egd=file:/dev/./urandom -DVAULT_TOKEN=$VAULT_TOKEN -Dsentry.dsn=$SENTRY_DSN -jar /app.jar' >> entrypoint.sh
 
 RUN chmod 555 entrypoint.sh app.jar
 
--- src/main/java/com/cyzest/findbooks/config/WebMvcConfig.java
+++ src/main/java/com/cyzest/findbooks/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package com.cyzest.findbooks.config;
 
+import io.sentry.spring.SentryServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
@@ -14,6 +18,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
         WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
         webContentInterceptor.setCacheSeconds(0);
         registry.addInterceptor(webContentInterceptor);
+    }
+
+    @Bean
+    @Profile("dev")
+    public ServletContextInitializer sentryServletContextInitializer() {
+        return new SentryServletContextInitializer();
     }
 
 }
