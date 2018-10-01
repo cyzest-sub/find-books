0df380497fecd5f6b7c11a50f03faa9ef307b725
cyzest <cyzest@nate.com>
2018-10-01 20:13:44 +0900

    Front-end 리소스 버저닝/캐싱 설정 추가 및 로그인 페이지 리펙토링

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,82 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
 });
+
+function login() {
+
+    var id = $.trim($("#id").val());
+    var password = $.trim($("#password").val());
+
+    if (id === "") {
+        $("#id").focus();
+        alert("아이디를 입력하세요.");
+        return false;
+    }
+
+    if (password === "") {
+        $("#password").focus();
+        alert("패스워드를 입력하세요.");
+        return false;
+    }
+
+    $.ajax({
+        url: "/login",
+        type: "POST",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: 'text',
+        data: jQuery.param({
+            username: id,
+            password: password
+        }),
+        success: function () {
+            window.location.href = "/";
+        },
+        error: function (error) {
+            alert("이메일이 없거나 비밀번호가 틀립니다.");
+            return false;
+        }
+    });
+
+    return false;
+}
 
 function register() {
 
+    var id = $('#reg_id').val();
     var password = $('#reg_password').val();
+    var confirmPassword = $('#confirm-password').val();
 
-    if (password !== $('#confirm_password').val()) {
-        alert("패스워드를 확인해주세요.");
+    if (id === '') {
+        $('#reg_id').focus();
+        alert("아이디를 입력하세요.");
+        return false;
+    }
+
+    if (password === '') {
+        $('#reg_password').focus();
+        alert("패스워드를 입력하세요.");
+        return false;
+    }
+
+    if (confirmPassword === '' || password !== confirmPassword) {
+        $('#confirm-password').focus();
+        alert("패스워드 확인을 해주세요.");
         return false;
     }
 
@@ -17,15 +86,15 @@ function register() {
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
         dataType: 'text',
         data: jQuery.param({
-            id: $('#reg_id').val(),
-            password: $('#reg_password').val()
+            id: id,
+            password: password
         }),
         success: function () {
-            alert("가입이 완료 되었습니다.");
-            location.href = "/";
+            window.location.href = "/";
         },
         error: function (error) {
-            console.log(error);
+            alert(error);
+            return false;
         }
     });
 
